67d31b6f0c55a6712088bacb0429a504d3e568db
zhuchentong <2037630@163.com>
2020-12-30 08:04:54 +0800

    update

--- app02/src/app/app.module.ts
+++ app02/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Page1Component, Page2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     {
